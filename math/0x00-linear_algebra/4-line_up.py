b22325466c8c39d1d49159bdf9b48fff1a93074a
nourouhichi <1417@holbertonschool.com>
2021-04-14 17:27:29 +0100

    new addd

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -6,6 +6,6 @@ module
 
 def add_arrays(arr1, arr2):
     """add"""
-    if len(arr1) != len(arr2):
+    if len(arr1) is not len(arr2):
         return None
     return [x + y for x, y in zip(arr1, arr2)]
