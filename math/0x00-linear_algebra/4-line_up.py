25273e0265bd83f03157dd3fbfda9aef02a0f2bc
nourouhichi <1417@holbertonschool.com>
2021-04-14 17:30:13 +0100

    new addd

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
-"""
-module
-"""
+"""Sum arrays"""
 
 
 def add_arrays(arr1, arr2):
-    """add"""
-    if len(arr1) is not len(arr2):
+    """ sum two arrays"""
+    if len(arr1) != len(arr2):
         return None
     return [x + y for x, y in zip(arr1, arr2)]
