77517109e92a54b7e6f7adc8fc0e36fcf86bcd36
nourouhichi <1417@holbertonschool.com>
2021-04-14 17:45:17 +0100

    fixing code

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -6,10 +6,13 @@ module
 
 def cat_matrices2D(mat1, mat2, axis=0):
     """concate matrices"""
-    mat = []
     if axis == 1:
-        for x in range(len(mat1)):
-            mat.append(mat1[x] + mat2[x])
+        if len(mat1) == len(mat2):
+            return [x + y for x, y in zip(mat1, mat2)]
+        else:
+            return None
     elif axis == 0:
-        mat = mat1 + mat2
-    return mat
+        if len(mat1[0]) == len(mat2[0]):
+            return [x.copy() for x in mat1] + [x.copy() for x in mat2]
+        else:
+            return None
