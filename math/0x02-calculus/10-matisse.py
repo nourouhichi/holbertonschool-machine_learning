05e39f411ec6cc4e6d4db4df2901096547d46558
nourouhichi <1417@holbertonschool.com>
2021-04-22 18:26:44 +0100

    new add

---
+++ math/0x02-calculus/10-matisse.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""module"""
+
+
+def poly_derivative(poly):
+    """function"""
+    if poly == [] or type(poly) is not list:
+        return None
+    if len(poly) == 1:
+        return poly[0]
+    deriv = []
+    for index in range(len(poly)):
+        deriv.append(index * poly[index])
+    deriv.pop(0)
+    return deriv
