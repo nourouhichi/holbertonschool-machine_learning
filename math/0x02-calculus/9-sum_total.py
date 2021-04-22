8b8c2d8f09f1e0a048aa9c76194617bcc1338120
nourouhichi <1417@holbertonschool.com>
2021-04-22 18:17:44 +0100

    new add

---
+++ math/0x02-calculus/9-sum_total.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""module"""
+
+
+def summation_i_squared(n):
+    """function"""
+    if type(n) is not int or n < 1:
+        return None
+    return int(n * (n + 1) * (2 * n + 1) / 6)
