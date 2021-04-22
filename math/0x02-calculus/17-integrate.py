c5077ce3f09198ae1cc060763f09319b1f4f4ae2
nourouhichi <1417@holbertonschool.com>
2021-04-22 20:21:22 +0100

    fixing

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -7,10 +7,13 @@ def poly_integral(poly, C=0):
     if poly == [] or type(poly) is not list or not isinstance(C, (float, int)):
         return None
     prim = [C]
-    if len(poly) > 1:
-        for index in range(len(poly)):
-            if (poly[index] / (index + 1)) % 1 == 0:
-                prim.append(int(poly[index] / (index + 1)))
-            else:
-                prim.append(poly[index] / (index + 1))
+    if len(poly) == 1 and poly[0] == 0:
+        return prim
+    for index in range(len(poly)):
+        if not isinstance(poly[index], (int, float)):
+            return None
+        if (poly[index] / (index + 1)) % 1 == 0:
+            prim.append(int(poly[index] / (index + 1)))
+        else:
+            prim.append(poly[index] / (index + 1))
     return prim
