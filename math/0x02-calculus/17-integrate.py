f401510ff8b36e2db4598adc74c992411c54e00f
nourouhichi <1417@holbertonschool.com>
2021-04-22 20:03:39 +0100

    new add

---
+++ math/0x02-calculus/17-integrate.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""module"""
+
+
+def poly_integral(poly, C=0):
+    """function"""
+    if poly == [] or type(poly) is not list or not isinstance(C, (float, int)):
+        return None
+    prim = [C]
+    if len(poly) > 1:
+        for index in range(len(poly)):
+            if (poly[index] / (index + 1)) % 1 == 0:
+                prim.append(int(poly[index] / (index + 1)))
+            else:
+                prim.append(poly[index] / (index + 1))
+    return prim
