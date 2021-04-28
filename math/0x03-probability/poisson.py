ae5c5555a3f3491c59703b7d18d967eb16a55139
nourouhichi <1417@holbertonschool.com>
2021-04-28 12:41:11 +0100

    new project

---
+++ math/0x03-probability/poisson.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""module"""
+
+
+class Poisson:
+    """poisson ditro class"""
+    def __init__(self, data=None, lambtha=1.):
+        """init  func"""
+        self.data = data
+        if data is  None:
+            if lambtha > 0:
+                self.lambtha = float(lambtha)
+            else:
+                raise ValueError("lambtha must be a positive value")
+        elif type(data) is not list:
+            raise TypeError("data must be a list")
+        elif len(data) < 2:
+            raise ValueError("data must contain multiple values")
+        else:
+            self.lambtha = float(sum(data) / len(data))
