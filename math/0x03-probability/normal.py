8363870cb7cf0ed0f469cc61e3e9f5e1ec64b775
nourouhichi <1417@holbertonschool.com>
2021-04-28 20:23:42 +0100

    new add

---
+++ math/0x03-probability/normal.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""module"""
+
+
+class Normal:
+    """normal distro"""
+
+    def __init__(self, data=None, mean=0., stddev=1.):
+        """ init fuc """
+        self.data = data
+        if data is None:
+            if stddev <= 0:
+                raise ValueError("stddev must be a positive value")
+            else:
+                self.stddev = float(stddev)
+                self.mean = float(mean)
+        else:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            elif len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            else:
+                self.mean = float(sum(data) / len(data))
+                y = 0
+                for i in data:
+                    y += (i - self.mean) ** 2
+                self.stddev = (y / len(data)) ** (1/2)
