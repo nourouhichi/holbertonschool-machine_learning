2645f19f6ee9193131588de2a04a21140d7d2f2e
nourouhichi <1417@holbertonschool.com>
2021-05-11 21:15:47 +0100

    new add

---
+++ supervised_learning/0x01-classification/2-neuron.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Neuron module
+"""
+import numpy as np
+
+
+class Neuron:
+    """ new class"""
+
+    def __init__(self, nx):
+        """init func"""
+        if type(nx) != int:
+            raise TypeError("nx must be an integer")
+        elif nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        return self.__W
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def A(self):
+        return self.__A
+
+    def forward_prop(self, X):
+        """forward propagation func"""
+        preact = np.dot(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-preact))
