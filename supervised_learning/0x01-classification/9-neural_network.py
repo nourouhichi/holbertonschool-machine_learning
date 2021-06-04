c87089a97e750e8035fc07caecb4e5cea4f0adf3
nourouhichi <1417@holbertonschool.com>
2021-06-04 12:59:31 +0100

    new add

---
+++ supervised_learning/0x01-classification/9-neural_network.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""
+Neuralnetwork  module
+"""
+import numpy as np
+
+
+class NeuralNetwork:
+    """ new class"""
+    def __init__(self, nx, nodes):
+        """init function"""
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(nodes) is not int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+        self.nx = nx
+        self.nodes = nodes
+        self.W1 = np.random.randn(nodes, nx)
+        self.b1 = np.zeros((nodes, 1))
+        self.A1 = 0
+        self.W2 = np.random.randn(1, nodes)
+        self.b2 = 0
+        self.A2 = 0
+
+    @property
+    def W1(self):
+        return self.__W1
+
+    @property
+    def b1(self):
+        return self.__b1
+
+    @property
+    def A1(self):
+        return self.__A1
+
+    @property
+    def W2(self):
+        return self.__W2
+
+    @property
+    def b2(self):
+        return self.__b2
+
+    @property
+    def A2(self):
+        return self.__A2
