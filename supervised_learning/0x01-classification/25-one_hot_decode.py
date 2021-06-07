ed2fd7bef190fe44f080e59a08520452bdd0dcec
nourouhichi <1417@holbertonschool.com>
2021-06-07 22:46:40 +0100

    new add

--- supervised_learning/0x01-classification/25-one_hot_decode.py
+++ supervised_learning/0x01-classification/25-one_hot_decode.py
@@ -7,7 +7,7 @@ import numpy as np
 
 def one_hot_decode(one_hot):
     """decoding"""
-    if type(one_hot) is not np.ndarray:
+    if type(one_hot) is not np.ndarray or one_hot.ndim is  not 2:
         return None
     try:
         x = np.zeros((len(one_hot),), dtype=int)
