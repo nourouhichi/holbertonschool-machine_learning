516e911a6023df1090ee2afac3a58fdf87c2340c
nourouhichi <1417@holbertonschool.com>
2021-06-07 22:50:19 +0100

    fix

--- supervised_learning/0x01-classification/25-one_hot_decode.py
+++ supervised_learning/0x01-classification/25-one_hot_decode.py
@@ -7,14 +7,6 @@ import numpy as np
 
 def one_hot_decode(one_hot):
     """decoding"""
-    if type(one_hot) is not np.ndarray or one_hot.ndim is  not 2:
+    if type(one_hot) is not np.ndarray:
         return None
-    try:
-        x = np.zeros((len(one_hot),), dtype=int)
-        for i in range(len(one_hot)):
-            for y in range(len(one_hot[i])):
-                if one_hot[i][y] == 1:
-                    x[y] = i
-        return x
-    except Exception:
-        return None
+    return np.argmax(one_hot.T, axis=1)
