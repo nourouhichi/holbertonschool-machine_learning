dbc91677406553a5eb237aa0c30893e780df1824
nourouhichi <1417@holbertonschool.com>
2021-06-07 22:08:32 +0100

    add exception

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
+"""new module"""
+
+
 import numpy as np
 
 
 def one_hot_encode(Y, classes):
-    one_hot_encoded = np.zeros((classes, len(Y)))
-    for i in range(len(Y)):
-        one_hot_encoded[Y[i], i] = 1
-    return one_hot_encoded
+    """encoding"""
+    try:
+        one_hot_encoded = np.zeros((classes, len(Y)))
+        for i in range(len(Y)):
+            one_hot_encoded[Y[i], i] = 1
+        return one_hot_encoded
+    except Exception:
+        return None
