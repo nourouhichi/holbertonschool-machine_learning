913714ee00551dd6e3183d8f3dd864dea9fc00b1
nourouhichi <1417@holbertonschool.com>
2021-06-07 01:59:30 +0100

    new add

---
+++ supervised_learning/0x01-classification/20-deep_neural_network.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+"""
+deep Neuralnetwork  module
+"""
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """ new class"""
+    def __init__(self, nx, layers):
+        """init function"""
+        if type(nx) != int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(layers) is not list or len(layers) == 0:
+            raise TypeError("layers must be a list of positive integers")
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+        for i in range(len(layers)):
+            if type(layers[i]) is not int or layers[i] <= 0:
+                raise TypeError("layers must be a list of positive integers")
+            if i == 0:
+                self.__weights['W1'] = \
+                    np.random.randn(layers[0], nx) * np.sqrt(2 / nx)
+            else:
+                self.__weights['W' + str(i + 1)] = \
+                    np.random.randn(layers[i], layers[i - 1]) * \
+                    np.sqrt(2 / layers[i - 1])
+            self.__weights['b' + str(i + 1)] = np.zeros((layers[i], 1))
+
+    @property
+    def L(self):
+        return self.__L
+
+    @property
+    def cache(self):
+        return self.__cache
+
+    @property
+    def weights(self):
+        return self.__weights
+
+    def forward_prop(self, X):
+        """ activation function"""
+        self.__cache["A0"] = X
+        for i in range(self.__L):
+            suc = str(i + 1)
+            ln = str(i)
+            z = np.matmul(self.__weights["W" + suc], self.__cache["A" + ln]) +\
+                self.__weights["b" + suc]
+            self.__cache["A" + str(suc)] = 1 / (1 + np.exp(- z))
+        return self.cache["A" + str(i + 1)], self.__cache
+
+    def cost(self, Y, A):
+        """loss function """
+        m = Y.shape[1]
+        z = 1.0000001 - A
+        L = (np.sum(Y * np.log(A) + (1 - Y) * np.log(z)))/m
+        return (-1 * L)
+
+    def evaluate(self, X, Y):
+        """evaluation function"""
+        self.forward_prop(X)
+        prediction = np.where(self.__cache["A" + str(self.__L)] < 0.5, 0, 1)
+        return prediction, self.cost(Y, self.__cache["A" + str(self.__L)])
