0fa422d476d3a4f69525fc79aa00de645df5b6f5
nourouhichi <1417@holbertonschool.com>
2021-06-07 22:43:14 +0100

    new add

---
+++ supervised_learning/0x01-classification/25-one_hot_decode.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""new module"""
+
+
+import numpy as np
+
+
+def one_hot_decode(one_hot):
+    """decoding"""
+    if type(one_hot) is not np.ndarray:
+        return None
+    try:
+        x = np.zeros((len(one_hot),), dtype=int)
+        for i in range(len(one_hot)):
+            for y in range(len(one_hot[i])):
+                if one_hot[i][y] == 1:
+                    x[y] = i
+        return x
+    except Exception:
+        return None
