32752030f4264364c0d6b6e620e1160c7ef67b8c
nourouhichi <1417@holbertonschool.com>
2021-06-09 00:35:07 +0100

    fixing

--- supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
+++ supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
@@ -8,10 +8,11 @@ def calculate_accuracy(y, y_pred):
     """acc function"""
     return tf.math.reduce_mean(
         tf.cast(
+            tf.math.equal(
                 tf.math.argmax(
                     y,
                     axis=-1),
                 tf.math.argmax(
                     y_pred,
                     axis=-1)),
-            tf.float32)
+            tf.float32))
