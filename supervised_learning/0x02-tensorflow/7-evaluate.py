6c6a4005899878897f32c53c94682be68f8f0106
nourouhichi <1417@holbertonschool.com>
2021-06-12 15:36:44 +0100

    new add

---
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python2
+"""evaluation"""
+
+import tensorflow as tf
+
+
+def evaluate(X, Y, save_path):
+    """return evaluation of the neural network"""
+
+    with tf.Session() as sess:
+        new_saver = tf.train.import_meta_graph(save_path + ".meta")
+        new_saver.restore(sess, save_path)
+        x = tf.get_collection('x')[0]
+        y = tf.get_collection('y')[0]
+        y_pred = tf.get_collection('y_pred')[0]
+        loss = tf.get_collection('loss')[0]
+        accuracy = tf.get_collection("accuracy")[0]
+        y_pred = sess.run(y_pred, feed_dict={x: X, y: Y})
+        cost = sess.run(loss, feed_dict={x: X, y: Y})
+        acc = sess.run(accuracy, feed_dict={x: X, y: Y})
+        return y_pred, acc, cost
