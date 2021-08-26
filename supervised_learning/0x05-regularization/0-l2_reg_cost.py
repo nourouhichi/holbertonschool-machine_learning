062d10da5b4431681a69966d558749b34d5d0bb2
nourouhichi <1417@holbertonschool.com>
2021-08-26 19:18:01 +0100

    new add

---
+++ supervised_learning/0x05-regularization/0-l2_reg_cost.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Reg """
+
+import numpy as np
+
+
+def l2_reg_cost(cost, lambtha, weights, L, m):
+    """ Calculates l2 reg cost func """
+    norm = 0
+    for i in range(1, L + 1):
+        norm += np.linalg.norm(weights["W" + str(i)])
+    return cost + (lambtha / (2 * m)) + norm
