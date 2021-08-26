67daa7137c7b3ec1fad0bb934ee3bea148a4e97b
nourouhichi <1417@holbertonschool.com>
2021-08-26 19:56:16 +0100

    fiw operator

--- supervised_learning/0x05-regularization/0-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/0-l2_reg_cost.py
@@ -8,5 +8,5 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
     """ Calculates l2 reg cost func """
     norm = 0
     for i in range(1, L + 1):
-        norm += np.linalg.norm(weights["W" + str(i)])
-    return cost + (lambtha / (2 * m)) + norm
+        norm += np.linalg.norm(weights["W" + str(i)]) ** 2
+    return cost + (lambtha / (2 * m)) * norm
