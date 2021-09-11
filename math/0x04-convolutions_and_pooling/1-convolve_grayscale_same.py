f099cb563620b8b8255392f6c0674450158bf446
nourouhichi <1417@holbertonschool.com>
2021-09-11 17:07:49 +0100

    new add

---
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""new module"""
+
+
+import numpy as np
+from math import ceil, floor
+
+
+def convolve_grayscale_same(images, kernel):
+    """same convolution"""
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+    if kh % 2 == 1:
+        pad_h = (kh - 1) // 2
+    else:
+        pad_h = kh // 2
+    if kw % 2 == 1:
+        pad_w = (kw - 1) // 2
+    else:
+        pad_w = kw // 2
+    output = np.zeros((m, h, w))
+    img_pad = np.pad(
+        array=images,
+        pad_width=((0,), (pad_h,), (pad_w,)),
+        mode="constant",
+        constant_values=0)
+
+    for x in range(h):
+        for y in range(w):
+            output[:, x, y] = (img_pad[:, x:kh+x, y:kw+y] * kernel
+                               ).sum(axis=(1, 2))
+    return output
