f4b228cf95f866a89228cd4454cd37a76ffe34a9
nourouhichi <1417@holbertonschool.com>
2021-09-26 11:27:46 +0100

    fix

--- supervised_learning/0x06-keras/1-input.py
+++ supervised_learning/0x06-keras/1-input.py
@@ -9,12 +9,11 @@ import tensorflow.keras as K
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """model building without using seq"""
     inputs = K.Input(shape=(nx,))
-    dense = K.layers.Dense(layers[0], activation=activations[0],
-                           kernel_regularizer=K.regularizers.l2(
-                           lambtha))
-    outputs = dense(inputs)
-    for i in range(1, len(layers)):
-        outputs = K.layers.Dropout(1 - keep_prob)(outputs)
+    outputs = inputs
+
+    for i in range(len(layers)):
+        if i != 0:
+            outputs = K.layers.Dropout(1 - keep_prob)(outputs)
         outputs = K.layers.Dense(layers[i],
                                  activation=activations[i],
                                  kernel_regularizer=K.regularizers.l2(
