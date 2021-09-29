f04457ade56c20083f32dd43fc570dfbb1f0f9b0
nourouhichi <1417@holbertonschool.com>
2021-09-29 13:58:27 +0100

    new add

---
+++ supervised_learning/0x07-cnn/0-conv_forward.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python34
+"""cnn"""
+
+
+import numpy as np
+
+
+def conv_forward(A_prev, W, b,
+                 activation, padding="same",
+                 stride=(1, 1)):
+    """forward prop in a convnet"""
+    m, h_prev, w_prev, c_prev = A_prev.shape
+    kh, kw, c_prev, c_new = W.shape
+    sh, sw = stride
+    if padding == 'valid':
+        pad_h = 0
+        pad_w = 0
+    elif padding == 'same':
+        pad_h = int((((h_prev - 1) * sh + kh - h_prev) / 2)) + 1
+        pad_w = int((((w_prev - 1) * sw + kw - w_prev) / 2)) + 1
+    output_h = int(((h_prev - kh + 2 * pad_h) / sh) + 1)
+    output_w = int(((w_prev - kw + 2 * pad_w) / sw) + 1)
+    output = np.zeros((m, output_h, output_w, c_new))
+    img_pad = np.pad(
+                     array=A_prev,
+                     pad_width=((0,), (pad_h,), (pad_w,), (0,)),
+                     mode="constant",
+                     constant_values=0)
+    for x in range(output_h):
+        for y in range(output_w):
+            for c in range(c_new):
+                piece = img_pad[:, x*sh:kh+x*sh, y*sw:kw+y*sw, :]
+                output[:, x, y, c] = activation(np.sum(
+                    piece*W[:, :, :, c], axis=(1, 2, 3)) + b[:, :, :, c])
+    return output
