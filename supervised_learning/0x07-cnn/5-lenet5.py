0c7b9220572b2538bd13652d4494b82403dfae2d
nourouhichi <1417@holbertonschool.com>
2021-10-03 13:51:49 +0100

    new add

---
+++ supervised_learning/0x07-cnn/5-lenet5.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+""" Convolutional Neural Networks """
+
+
+import tensorflow.keras as K
+from tensorflow.python.ops.init_ops import Initializer
+
+
+def lenet5(X):
+    """lenet5 with keras"""
+    initializer = K.initializers.he_normal()
+    c1 = K.layers.Conv2D(6, (5, 5),
+                         padding='same', kernel_initializer=initializer,
+                         activation='relu')(X)
+    p1 = K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(c1)
+    c2 = K.layers.Conv2D(16, (5, 5),
+                         padding='valid', kernel_initializer=initializer,
+                         activation='relu')(p1)
+    p2 = K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(c2)
+    l1 = K.layers.Dense(120, kernel_initializer=initializer,
+                        activation='relu')(K.layers.Flatten()(p2))
+    l2 = K.layers.Dense(84, kernel_initializer=initializer,
+                        activation='relu')(l1)
+    output = K.layers.Dense(10, kernel_initializer=initializer,
+                            activation='softmax')(l2)
+    model = K.models.Model(inputs=X, outputs=output)
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=K.optimizers.Adam(),
+                  metrics=['accuracy'])
+    return model
