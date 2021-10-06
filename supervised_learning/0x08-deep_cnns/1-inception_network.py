216e21f86d5883d7aba07fae992731d8582a4ba8
nourouhichi <1417@holbertonschool.com>
2021-10-06 12:01:26 +0100

    pycodestyle fix

--- supervised_learning/0x08-deep_cnns/1-inception_network.py
+++ supervised_learning/0x08-deep_cnns/1-inception_network.py
@@ -7,35 +7,34 @@ inception_block = __import__('0-inception_block').inception_block
 def inception_network():
     """ Build cnn"""
     input = K.Input(shape=(224, 224, 3))
-    l = K.layers.Conv2D(64, (7, 7), strides=(
+    layer = K.layers.Conv2D(64, (7, 7), strides=(
         2, 2), padding='same', activation='relu')(input)
-    l = K.layers.MaxPooling2D(
+    layer = K.layers.MaxPooling2D(
         (3, 3), strides=(
-            2, 2), padding='same')(l)
-    l = K.layers.Conv2D(64, (1, 1), strides=(
-        1, 1), padding='same', activation='relu')(l)
-    l = K.layers.Conv2D(192, (3, 3), strides=(
-        1, 1), padding='same', activation='relu')(l)
-    l = K.layers.MaxPooling2D(
+            2, 2), padding='same')(layer)
+    layer = K.layers.Conv2D(64, (1, 1), strides=(
+        1, 1), padding='same', activation='relu')(layer)
+    layer = K.layers.Conv2D(192, (3, 3), strides=(
+        1, 1), padding='same', activation='relu')(layer)
+    layer = K.layers.MaxPooling2D(
         (3, 3), strides=(
-            2, 2), padding='same')(l)
-    l = inception_block(l, (64, 96, 128, 16, 32, 32))
-    l = inception_block(l, (128, 128, 192, 32, 96, 64))
-    l = K.layers.MaxPooling2D(
+            2, 2), padding='same')(layer)
+    layer = inception_block(layer, (64, 96, 128, 16, 32, 32))
+    layer = inception_block(layer, (128, 128, 192, 32, 96, 64))
+    layer = K.layers.MaxPooling2D(
         (3, 3), strides=(
-            2, 2), padding='same')(l)
-    l = inception_block(l, (192, 96, 208, 16, 48, 64))
-    l = inception_block(l, (160, 112, 224, 24, 64, 64))
-    l = inception_block(l, (128, 128, 256, 24, 64, 64))
-    l = inception_block(l, (112, 144, 288, 32, 64, 64))
-    l = inception_block(l, (256, 160, 320, 32, 128, 128))
-    l = K.layers.MaxPooling2D(
+            2, 2), padding='same')(layer)
+    layer = inception_block(layer, (192, 96, 208, 16, 48, 64))
+    layer = inception_block(layer, (160, 112, 224, 24, 64, 64))
+    layer = inception_block(layer, (128, 128, 256, 24, 64, 64))
+    layer = inception_block(layer, (112, 144, 288, 32, 64, 64))
+    layer = inception_block(layer, (256, 160, 320, 32, 128, 128))
+    layer = K.layers.MaxPooling2D(
         (3, 3), strides=(
-            2, 2), padding='same')(l)
-    l = inception_block(l, (256, 160, 320, 32, 128, 128))
-    l = inception_block(l, (384, 192, 384, 48, 128, 128))
-    l = K.layers.AveragePooling2D((7, 7), strides=1)(l)
-    l = K.layers.Dropout(0.4)(l)
-    output = K.layers.Dense(1000, activation='softmax')(l)
-    model = K.models.Model(inputs=input, outputs=output)
-    return model
+            2, 2), padding='same')(layer)
+    layer = inception_block(layer, (256, 160, 320, 32, 128, 128))
+    layer = inception_block(layer, (384, 192, 384, 48, 128, 128))
+    layer = K.layers.AveragePooling2D((7, 7), strides=1)(layer)
+    layer = K.layers.Dropout(0.4)(layer)
+    output = K.layers.Dense(1000, activation='softmax')(layer)
+    return K.models.Model(inputs=input, outputs=output)
--- supervised_learning/0x08-deep_cnns/2-identity_block.py
+++ supervised_learning/0x08-deep_cnns/2-identity_block.py
@@ -6,29 +6,28 @@ import tensorflow.keras as K
 def identity_block(A_prev, filters):
     """ Build an identity block """
     initializer = K.initializers.he_normal()
-    l = K.layers.Conv2D(
+    layer = K.layers.Conv2D(
         filters[0],
         (1,
          1),
         padding='same',
         kernel_initializer=initializer)(A_prev)
-    l = K.layers.BatchNormalization()(l)
-    l = K.layers.Activation('relu')(l)
-    l = K.layers.Conv2D(
+    layer = K.layers.BatchNormalization()(layer)
+    layer = K.layers.Activation('relu')(layer)
+    layer = K.layers.Conv2D(
         filters[1],
         (3,
          3),
         padding='same',
-        kernel_initializer=initializer)(l)
-    l = K.layers.BatchNormalization()(l)
-    l = K.layers.Activation('relu')(l)
-    l = K.layers.Conv2D(
+        kernel_initializer=initializer)(layer)
+    layer = K.layers.BatchNormalization()(layer)
+    layer = K.layers.Activation('relu')(layer)
+    layer = K.layers.Conv2D(
         filters[2],
         (1,
          1),
         padding='same',
-        kernel_initializer=initializer)(l)
-    l = K.layers.BatchNormalization()(l)
-    output = K.layers.Add()([l, A_prev])
-    output = K.layers.Activation('relu')(output)
-    return output
+        kernel_initializer=initializer)(layer)
+    layer = K.layers.BatchNormalization()(layer)
+    output = K.layers.Add()([layer, A_prev])
+    return K.layers.Activation('relu')(output)
