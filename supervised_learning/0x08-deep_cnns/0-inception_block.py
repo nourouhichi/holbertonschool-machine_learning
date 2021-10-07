a11984705693aaca12427ebb8be1c66860ae4e96
nourouhichi <1417@holbertonschool.com>
2021-10-07 18:25:44 +0100

    new add

--- supervised_learning/0x08-deep_cnns/0-inception_block.py
+++ supervised_learning/0x08-deep_cnns/0-inception_block.py
@@ -5,20 +5,36 @@ import tensorflow.keras as K
 
 def inception_block(A_prev, filters):
     """ inception block """
-    c1 = K.layers.Conv2D(
-        filters[0], (1, 1), padding='same', activation='relu')(A_prev)
-    c1_3 = K.layers.Conv2D(
-        filters[1], (1, 1), padding='same', activation='relu')(A_prev)
-    c3 = K.layers.Conv2D(
-        filters[2], (3, 3), padding='same', activation='relu')(c1_3)
-    c1_5 = K.layers.Conv2D(
-        filters[3], (1, 1), padding='same', activation='relu')(A_prev)
-    c5 = K.layers.Conv2D(
-        filters[4], (5, 5), padding='same', activation='relu')(c1_5)
-    pool = K.layers.MaxPooling2D(
-        (3, 3), strides=(
-            1, 1), padding='same')(A_prev)
-    convp_1 = K.layers.Conv2D(
-        filters[5], (1, 1), padding='same', activation='relu')(pool)
-    output = K.layers.concatenate([c1, c3, c5, convp_1], axis=-1)
-    return output
+    F1 = K.layers.Conv2D(
+                         filters[0],
+                         (1, 1),
+                         padding="same",
+                         activation="relu")(A_prev)
+    F3R = K.layers.Conv2D(
+                         filters[1],
+                         (1, 1),
+                         padding="same",
+                         activation="relu")(A_prev)
+    F3 = K.layers.Conv2D(
+                         filters[2],
+                         (3, 3),
+                         padding="same",
+                         activation="relu")(F3R)
+    F5R = K.layers.Conv2D(
+                          filters[3],
+                          (1, 1),
+                          padding="same",
+                          activation="relu")(A_prev)
+    F5 = K.layers.Conv2D(
+                          filters[4],
+                          (5, 5),
+                          padding="same",
+                          activation="relu")(F5R)
+    pool = K.layers.MaxPooling2D(pool_size=(3, 3), strides=(1, 1),
+                                 padding="same")(A_prev)
+    FPP = K.layers.Conv2D(
+                          filters[5],
+                          (1, 1),
+                          padding="same",
+                          activation="relu")(pool)
+    return K.layers.Concatenate(axis=-1)([F1, F3, F5, FPP])
