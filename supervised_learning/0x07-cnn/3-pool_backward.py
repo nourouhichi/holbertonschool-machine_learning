7388fb920d71108068b9f951b13eb11724a5dfdc
nourouhichi <1417@holbertonschool.com>
2021-10-10 22:27:38 +0100

    recheck

--- supervised_learning/0x07-cnn/3-pool_backward.py
+++ supervised_learning/0x07-cnn/3-pool_backward.py
@@ -1,27 +1,26 @@
 #!/usr/bin/env python3
 """ Convolutional Neural Networks """
 import numpy as np
+from numpy.core.numeric import zeros_like
 
 
-def pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='avg'):
-    """perform back propagation over a pooling layer"""
+def pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='max'):
+    """backpropagation over a pooling layer"""
     m, h, w, cnew = A_prev.shape
     m, hnew, wnew, cnew = dA.shape
     kh, kw = kernel_shape
     sh, sw = stride
-    dA_prev = np.zeros_like(A_prev)
-
-    for i in range(m):
+    da_prev = np.zeros_like(A_prev)
+    for z in range(m):
         for x in range(hnew):
             for y in range(wnew):
                 for c in range(cnew):
-                    A = A_prev[i, x*sh:kh+x*sh, y*sw:kw+y*sw, c]
-                    dAu = dA_prev[i, x*sh:kh+x*sh, y*sw:kw+y*sw, c]
-
-                    if mode == 'max':
-                        mask = (A == np.max(A))
-                        dAu += np.multiply(dA[i, x, y, c], mask)
-
+                    if mode == "max":
+                        slice = A_prev[z, x*sh:x*sh+kh, y*sw:y*sw+kw, c]
+                        bool = (np.max(slice) == slice)
+                        da_prev[z, x*sh:x*sh+kh, y*sw:y *
+                                sw+kw, c] += np.multiply(bool, dA[z, x, y, c])
                     if mode == 'avg':
-                        dAu += dA[i, x, y, c]/(kh*kw)
-    return dA_prev
+                        da_prev[z, x * sh:x * sh + kh, y *
+                                sw:y * sw + kw, c] += dA[z, x, y, c] / kh / kw
+    return da_prev
