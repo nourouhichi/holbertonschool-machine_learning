ebb00bc46ac628ad5f2945e7c8c74722534c371c
nourouhichi <1417@holbertonschool.com>
2021-10-11 13:10:00 +0100

    recheck

--- supervised_learning/0x07-cnn/5-lenet5.py
+++ supervised_learning/0x07-cnn/5-lenet5.py
@@ -3,27 +3,27 @@
 
 
 import tensorflow.keras as K
+from tensorflow.python.keras.layers.core import Activation
 
 
 def lenet5(X):
     """lenet5 with keras"""
-    initializer = K.initializers.he_normal()
-    c1 = K.layers.Conv2D(6, (5, 5),
-                         padding='same', kernel_initializer=initializer,
-                         activation='relu')(X)
+    m, x, y, z = X.shape
+    init = K.initializers.he_normal()
+    c1 = K.layers.Conv2D(6, (5, 5), padding="same",
+                         activation="relu",
+                         kernel_initializer=init)(X)
     p1 = K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(c1)
-    c2 = K.layers.Conv2D(16, (5, 5),
-                         padding='valid', kernel_initializer=initializer,
-                         activation='relu')(p1)
+    c2 = K.layers.Conv2D(16, (5, 5), padding="valid",
+                         activation="relu", kernel_initializer=init)(p1)
     p2 = K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(c2)
-    l1 = K.layers.Dense(120, kernel_initializer=initializer,
-                        activation='relu')(K.layers.Flatten()(p2))
-    l2 = K.layers.Dense(84, kernel_initializer=initializer,
-                        activation='relu')(l1)
-    output = K.layers.Dense(10, kernel_initializer=initializer,
-                            activation='softmax')(l2)
-    model = K.models.Model(inputs=X, outputs=output)
-    model.compile(loss='categorical_crossentropy',
-                  optimizer=K.optimizers.Adam(),
+    fc1 = K.layers.Dense(120, activation="relu",
+                         kernel_initializer=init)(K.layers.Flatten()(p2))
+    fc2 = K.layers.Dense(84, activation="relu", kernel_initializer=init)(fc1)
+    y_pred = K.layers.Dense(10, kernel_initializer=init,
+                            activation="softmax")(fc2)
+    model = K.Model(inputs=X, outputs=y_pred)
+    model.compile(optimizer="Adam",
+                  loss='categorical_crossentropy',
                   metrics=['accuracy'])
     return model
