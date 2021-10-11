f1f12222e267619c0c4d50b334fd9fcd4c92b9af
nourouhichi <60342914+nourouhichi@users.noreply.github.com>
2021-10-11 22:49:51 +0100

    Update 14-batch_norm.py

--- supervised_learning/0x03-optimization/14-batch_norm.py
+++ supervised_learning/0x03-optimization/14-batch_norm.py
@@ -12,8 +12,8 @@ def create_batch_norm_layer(prev, n, activation):
             mode="FAN_AVG"),
         name="layer")(prev)
     mean, v = tf.nn.moments(layer, axes=[0])
-    g = tf.ones([n])
-    b = tf.zeros([n])
+    gamma = tf.ones([n])
+    beta = tf.zeros([n])
     normalized = tf.nn.batch_normalization(
-        layer, mean, v, b, g, 1e-8)
+        layer, mean, v, beta, gamma, 1e-8)
     return activation(normalized)
