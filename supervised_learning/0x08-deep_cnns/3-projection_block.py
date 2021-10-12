a1df0f6c0317330d09ccba2ec490c1cadfb88f4a
nourouhichi <1417@holbertonschool.com>
2021-10-12 16:20:52 +0100

    recheck

--- supervised_learning/0x08-deep_cnns/3-projection_block.py
+++ supervised_learning/0x08-deep_cnns/3-projection_block.py
@@ -5,37 +5,35 @@ import tensorflow.keras as K
 
 def projection_block(A_prev, filters, s=2):
     """ projection block """
-    initializer = K.initializers.he_normal()
-    layer = K.layers.Conv2D(
-        filters[0],
-        (1,
-         1), strides=s,
-        padding='same',
-        kernel_initializer=initializer)(A_prev)
-    layer = K.layers.BatchNormalization()(layer)
-    layer = K.layers.Activation('relu')(layer)
-    layer = K.layers.Conv2D(
-        filters[1],
-        (3,
-         3),
-        padding='same',
-        kernel_initializer=initializer)(layer)
-    layer = K.layers.BatchNormalization()(layer)
-    layer = K.layers.Activation('relu')(layer)
-    layer = K.layers.Conv2D(
-        filters[2],
-        (1,
-         1),
-        padding='same',
-        kernel_initializer=initializer)(layer)
-    layer = K.layers.BatchNormalization()(layer)
-    A_prev = K.layers.Conv2D(
-        filters[2],
-        (1,
-         1), strides=s,
-        padding='same',
-        kernel_initializer=initializer)(A_prev)
-    A_prev = K.layers.BatchNormalization()(A_prev)
-    output = K.layers.Add()([layer, A_prev])
+    init = K.initializers.he_normal()
+    c1 = K.layers.Conv2D(
+                        filters[0],
+                        (1, 1),
+                        strides=s,
+                        padding='same',
+                        kernel_initializer=init)(A_prev)
+    norm1 = K.layers.BatchNormalization()(c1)
+    l1 = K.layers.Activation('relu')(norm1)
+    c2 = K.layers.Conv2D(
+                        filters[1],
+                        (3, 3),
+                        padding='same',
+                        kernel_initializer=init)(l1)
+    norm2 = K.layers.BatchNormalization()(c2)
+    l2 = K.layers.Activation('relu')(norm2)
+    c3 = K.layers.Conv2D(
+                        filters[2],
+                        (1, 1),
+                        padding='same',
+                        kernel_initializer=init)(l2)
+    norm3 = K.layers.BatchNormalization()(c3)
+    c4 = K.layers.Conv2D(
+                        filters[2],
+                        (1, 1),
+                        strides=s,
+                        padding='same',
+                        kernel_initializer=init)(A_prev)
+    norm4 = K.layers.BatchNormalization()(c4)
+    output = K.layers.Add()([norm3, norm4])
     output = K.layers.Activation('relu')(output)
     return output
