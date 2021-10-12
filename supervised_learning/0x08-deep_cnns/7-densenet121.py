3d302b62e8a881185be41c070a15735391d38fc1
nourouhichi <1417@holbertonschool.com>
2021-10-12 21:43:57 +0100

    recheck

--- supervised_learning/0x08-deep_cnns/7-densenet121.py
+++ supervised_learning/0x08-deep_cnns/7-densenet121.py
@@ -7,16 +7,19 @@ transition_layer = __import__('6-transition_layer').transition_layer
 
 def densenet121(growth_rate=32, compression=1.0):
     """DenseNet-121 architecture """
-    input_layer = K.Input(shape=(224, 224, 3))
-    layer = K.layers.BatchNormalization(axis=3)(input_layer)
-    layer = K.layers.Activation('relu')(layer)
-    layer = K.layers.Conv2D(2 * growth_rate, (7, 7), (2, 2),
-                            kernel_initializer='he_normal',
-                            padding='same')(layer)
-    layer = K.layers.MaxPooling2D(pool_size=(3, 3),
-                                  strides=2,
-                                  padding="same")(layer)
-    layer, nb_filters = dense_block(layer, 2 * growth_rate, growth_rate, 6)
+    init = K.initializers.he_normal()
+    input = K.Input(shape=(224, 224, 3))
+    norm = K.layers.BatchNormalization(axis=3)(input)
+    ly = K.layers.Activation('relu')(norm)
+    c = K.layers.Conv2D(2 * growth_rate,
+                        (7, 7),
+                        (2, 2),
+                        kernel_initializer=init,
+                        padding='same')(ly)
+    p = K.layers.MaxPooling2D(pool_size=(3, 3),
+                              strides=2,
+                              padding="same")(c)
+    layer, nb_filters = dense_block(p, 2 * growth_rate, growth_rate, 6)
     layer, nb_filters = transition_layer(layer, nb_filters, compression)
     layer, nb_filters = dense_block(layer, nb_filters, growth_rate, 12)
     layer, nb_filters = transition_layer(layer, nb_filters, compression)
@@ -26,5 +29,5 @@ def densenet121(growth_rate=32, compression=1.0):
     layer = K.layers.AveragePooling2D(pool_size=(7, 7),
                                       strides=(1, 1))(layer)
     output = K.layers.Dense(units=1000, activation='softmax',
-                            kernel_initializer='he_normal')(layer)
-    return K.models.Model(inputs=input_layer, outputs=output)
+                            kernel_initializer=init)(layer)
+    return K.models.Model(inputs=input, outputs=output)
