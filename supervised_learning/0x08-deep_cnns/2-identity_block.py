d9a5b997d3dec605672938655953347732402971
nourouhichi <1417@holbertonschool.com>
2021-10-12 15:35:28 +0100

    recheck

--- supervised_learning/0x08-deep_cnns/2-identity_block.py
+++ supervised_learning/0x08-deep_cnns/2-identity_block.py
@@ -1,33 +1,30 @@
 #!/usr/bin/env python3
-""" Identity Block module"""
+""" indetity cnn"""
 import tensorflow.keras as K
 
 
 def identity_block(A_prev, filters):
-    """ Build an identity block """
-    initializer = K.initializers.he_normal()
-    layer = K.layers.Conv2D(
-        filters[0],
-        (1,
-         1),
-        padding='same',
-        kernel_initializer=initializer)(A_prev)
-    layer = K.layers.BatchNormalization()(layer)
-    layer = K.layers.Activation('relu')(layer)
-    layer = K.layers.Conv2D(
-        filters[1],
-        (3,
-         3),
-        padding='same',
-        kernel_initializer=initializer)(layer)
-    layer = K.layers.BatchNormalization()(layer)
-    layer = K.layers.Activation('relu')(layer)
-    layer = K.layers.Conv2D(
-        filters[2],
-        (1,
-         1),
-        padding='same',
-        kernel_initializer=initializer)(layer)
-    layer = K.layers.BatchNormalization()(layer)
-    output = K.layers.Add()([layer, A_prev])
-    return K.layers.Activation('relu')(output)
+    """building identity block"""
+    init = K.initializers.he_normal()
+    c1 = K.layers.Conv2D(
+                         filters[0],
+                         (1, 1),
+                         padding='same',
+                         kernel_initializer=init)(A_prev)
+    norm1 = K.layers.BatchNormalization(axis=-1)(c1)
+    l1 = K.layers.Activation('relu')(norm1)
+    c2 = K.layers.Conv2D(
+                         filters[1],
+                         (3, 3),
+                         padding='same',
+                         kernel_initializer=init)(l1)
+    norm2 = K.layers.BatchNormalization(axis=-1)(c2)
+    l2 = K.layers.Activation('relu')(norm2)
+    c3 = K.layers.Conv2D(
+                         filters[2],
+                         (1, 1),
+                         padding='same',
+                         kernel_initializer=init)(l2)
+    norm3 = K.layers.BatchNormalization(axis=-1)(c3)
+    l3 = K.layers.Activation('relu')(norm3)
+    return l3
