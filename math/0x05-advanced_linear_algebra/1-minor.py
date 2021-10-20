bb58e55eefe4e3e6fadddfa86ba7ad17e45a31db
nourouhichi <1417@holbertonschool.com>
2021-10-20 20:24:58 +0100

    new try

--- math/0x05-advanced_linear_algebra/1-minor.py
+++ math/0x05-advanced_linear_algebra/1-minor.py
@@ -4,9 +4,17 @@
 
 def determinant(matrix):
     """det matrix calculation"""
-    ind = list(range(len(matrix)))
     if matrix == [[]]:
         return 1
+    if type(matrix) != list or matrix == []:
+        raise TypeError('matrix must be a list of lists')
+    for i in range(len(matrix)):
+        if len(matrix) != len(matrix[i]):
+            raise ValueError('matrix must be a square matrix')
+    for i in matrix:
+        if type(i) is not list:
+            raise TypeError('matrix must be a list of lists')
+    ind = list(range(len(matrix)))
     if len(matrix) == 1:
         return matrix[0][0]
     if len(matrix) == 2 and len(matrix[0]) == 2:
