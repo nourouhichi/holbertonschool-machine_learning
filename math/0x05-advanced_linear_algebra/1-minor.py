00afb71a7b02a4ee5472c47a6f25fd0eb2978628
nourouhichi <1417@holbertonschool.com>
2021-10-20 20:14:00 +0100

    new try

--- math/0x05-advanced_linear_algebra/1-minor.py
+++ math/0x05-advanced_linear_algebra/1-minor.py
@@ -2,16 +2,37 @@
 """minor advanced linear algb"""
 
 
-def minor_det(m, i, j):
-    """determinant of the slice"""
-    m1 = m.copy()
-    return [row[:j] + row[j+1:] for row in (m1[:i]+m1[i+1:])]
+def determinant(matrix):
+    """det matrix calculation"""
+    if matrix == [[]]:
+        return 1
+    if type(matrix) != list or matrix == []:
+        raise TypeError('matrix must be a list of lists')
+    for i in range(len(matrix)):
+        if len(matrix) != len(matrix[i]):
+            raise ValueError('matrix must be a square matrix')
+    for i in matrix:
+        if type(i) is not list:
+            raise TypeError('matrix must be a list of lists')
+    ind = list(range(len(matrix)))
+    if len(matrix) == 1:
+        return matrix[0][0]
+    if len(matrix) == 2 and len(matrix[0]) == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1]
+    else:
+        det = 0
+        for i in ind:
+            focus = matrix.copy()
+            focus = focus[1:]
+            h = len(focus)
+            for j in range(h):
+                focus[j] = focus[j][0:i] + focus[j][i+1:]
+            det += (-1) ** (i % 2) * matrix[0][i] * determinant(focus)
+    return det
 
 
 def minor(matrix):
     """minor matrix """
-    if matrix == [[]]:
-        raise ValueError('matrix must be a non-empty square')
     if type(matrix) != list or matrix == []:
         raise TypeError('matrix must be a list of lists')
     for i in range(len(matrix)):
@@ -26,12 +47,16 @@ def minor(matrix):
         m.append(matrix[0])
         return m
     if len(matrix) == 1:
-        return 1
+        return [[1]]
     minor_m = []
     for i in range(len(matrix)):
         m_m = []
         for j in range(len(matrix[0])):
-            m = minor_det(matrix, i, j)
-            m_m.append(m[0][0] * m[1][1] - m[1][0] * m[0][1])
+            m = [y[:] for y in matrix]
+            del m[i]
+            for z in m:
+                del z[j]
+            det = determinant(m)
+            m_m.append(det)
         minor_m.append(m_m)
     return minor_m
