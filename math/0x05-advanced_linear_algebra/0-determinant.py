4a1c387fff412fae831c16f2aee2aa966d23fc24
nourouhichi <1417@holbertonschool.com>
2021-10-20 18:37:21 +0100

    i cant see the problem

--- math/0x05-advanced_linear_algebra/0-determinant.py
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -6,13 +6,13 @@ def determinant(matrix):
     if matrix == [[]]:
         return 1
     if type(matrix) != list or matrix == []:
-        raise TypeError("matrix must be a list of lists")
+        raise TypeError('matrix must be a list of lists')
     for i in range(len(matrix)):
         if len(matrix) != len(matrix[i]):
             raise ValueError('matrix must be a square matrix')
     for i in matrix:
         if type(i) is not list:
-            raise TypeError("matrix must be a list of lists")
+            raise TypeError('matrix must be a list of lists')
     ind = list(range(len(matrix)))
     if len(matrix) == 1:
         return matrix[0][0]
