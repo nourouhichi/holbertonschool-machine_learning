4874ebd851ae9970824fb7c3f3a7c99968fc5b90
nourouhichi <1417@holbertonschool.com>
2021-10-20 18:18:21 +0100

    new project

---
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""det"""
+
+
+def determinant(matrix):
+    """det"""
+    if matrix == [[]]:
+        return 1
+    if type(matrix) != list or matrix == []:
+        raise TypeError("matrix must be a list of lists")
+    if len(matrix) != len(matrix[0]):
+        raise ValueError('matrix must be a square matrix')
+    ind = list(range(len(matrix)))
+    if len(matrix) == 1:
+        return matrix[0][0]
+    if len(matrix) == 2 and len(matrix[0]) == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1]
+    else:
+        det = 0
+        for i in ind:
+            focus = matrix.copy()
+            focus = focus[1:]
+            h = len(focus)
+            for j in range(h):
+                focus[j] = focus[j][0:i] + focus[j][i+1:]
+            det += (-1) ** (i % 2) * matrix[0][i] * determinant(focus)
+    return det
