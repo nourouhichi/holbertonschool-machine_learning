2c2d7c9067e22e2b9120a0f678ce9d37fef3f65c
nourouhichi <1417@holbertonschool.com>
2021-10-25 18:47:16 +0100

    add cond

--- math/0x06-multivariate_prob/0-mean_cov.py
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -5,7 +5,7 @@ import numpy as np
 
 def mean_cov(X):
     """mean cov"""
-    if len(X.shape) != 2:
+    if len(X.shape) != 2 or type(X) != np.ndarray:
         raise TypeError("X must be a 2D numpy.ndarray")
     elif X.shape[0] < 2:
         raise ValueError("X must contain multiple data points")
