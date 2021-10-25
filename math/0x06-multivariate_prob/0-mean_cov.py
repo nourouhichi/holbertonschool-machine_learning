8e173d88fe3917abded64bbf1d5d0e5d9c02b008
nourouhichi <1417@holbertonschool.com>
2021-10-25 18:41:23 +0100

    new add

--- math/0x06-multivariate_prob/0-mean_cov.py
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -11,5 +11,5 @@ def mean_cov(X):
         raise ValueError("X must contain multiple data points")
     mean = np.mean(X, axis=0)
     X -= mean
-    cov = np.dot(X.T, X.conj()/(X.shape[0] - 1))
+    cov = np.dot(X.T, X/(X.shape[0] - 1))
     return mean, cov
