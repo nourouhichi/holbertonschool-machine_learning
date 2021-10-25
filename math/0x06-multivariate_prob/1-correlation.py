a8046d0672b1a9e419afe53962fdd61fa432ba14
nourouhichi <1417@holbertonschool.com>
2021-10-25 19:40:57 +0100

    new add

---
+++ math/0x06-multivariate_prob/1-correlation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""probability"""
+import numpy as np
+
+
+def correlation(C):
+    """corr matrix"""
+    if type(C) != np.ndarray:
+        raise TypeError("C must be a numpy.ndarray")
+    if len(C.shape) != 2 and C.shape[0] != C.shape[1]:
+        raise ValueError("C must be a 2D square matrix")
+    d = C.shape[0]
+    corr = np.zeros((d, d))
+    for i in range(d):
+        for j in range(d):
+            corr[i][j] = C[i][j] / np.sqrt(C[i][i] * C[j][j])
+    return corr
