469ee107d15c562f6d77fc2c66649a03efae07b1
nourouhichi <1417@holbertonschool.com>
2021-10-25 18:34:16 +0100

    new add

---
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Gaussian distro"""
+import numpy as np
+
+
+def mean_cov(X):
+    """mean cov"""
+    if len(X.shape) != 2:
+        raise TypeError("X must be a 2D numpy.ndarray")
+    elif X.shape[0] < 2:
+        raise ValueError("X must contain multiple data points")
+    mean = np.mean(X, axis=0)
+    X -= mean
+    cov = np.dot(X.T, X.conj()/X.shape[0])
+    return mean, cov
