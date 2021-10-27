d09841ea0a9bb336f56fff58a7d1ba8ed0ecdd3d
nourouhichi <1417@holbertonschool.com>
2021-10-27 18:28:04 +0100

    new add

--- unsupervised_learning/0x00-dimensionality_reduction/0-pca.py
+++ unsupervised_learning/0x00-dimensionality_reduction/0-pca.py
@@ -5,8 +5,10 @@ import numpy as np
 
 def pca(X, var=0.95):
     """feature extraction"""
-    x = X.copy()
-    cov = np.cov(x.T)
-    eigen_va, eigen_ve = np.linalg.eig(cov)
-    proj = (eigen_ve.T[:][:2]).T
-    return proj
+    u, sig, v = np.linalg.svd(X)
+    cov = np.cumsum(sig) / np.sum(sig)
+    i = np.where(cov <= var, 1, 0)
+    print(cov)
+    print(i)
+    i = np.sum(i)
+    return v.T[:, :i]
