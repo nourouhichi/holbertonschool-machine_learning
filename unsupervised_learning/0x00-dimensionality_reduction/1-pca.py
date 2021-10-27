85e213f422cdc5baf3419ed289fc37866a5435e3
nourouhichi <1417@holbertonschool.com>
2021-10-27 20:05:04 +0100

    add axis

--- unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
+++ unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
@@ -5,6 +5,6 @@ import numpy as np
 
 def pca(X, ndim):
     """pca function"""
-    mean_sub = X - np.mean(X)
+    mean_sub = X - np.mean(X, axis=0)
     u, sig, v = np.linalg.svd(mean_sub)
     return np.matmul(mean_sub, v.T[:, :ndim])
