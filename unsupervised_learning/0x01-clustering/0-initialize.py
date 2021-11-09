a5a0db90a73b31303ede6d35f07f38c92c1f3402
nourouhichi <1417@holbertonschool.com>
2021-11-09 11:46:22 +0100

    new try

--- unsupervised_learning/0x01-clustering/0-initialize.py
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -7,9 +7,9 @@ def initialize(X, k):
     """random centroids"""
     if type(k) != int or k <= 0:
         return None
-    maxi = np.amax(X, axis=0)
-    mini = np.amin(X, axis=0)
     try:
+        maxi = np.amax(X, axis=0)
+        mini = np.amin(X, axis=0)
         return np.random.uniform(mini, maxi, (k, X.shape[1]))
     except Exception:
         return None
