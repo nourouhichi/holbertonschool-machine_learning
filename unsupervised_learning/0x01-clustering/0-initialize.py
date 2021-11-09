915e6437e52215e92b117fb5661fc44b3d789ccf
nourouhichi <1417@holbertonschool.com>
2021-11-09 11:41:51 +0100

    new add

---
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""clustring"""
+import numpy as np
+
+
+def initialize(X, k):
+    """random centroids"""
+    maxi = np.amax(X, axis=0)
+    mini = np.amin(X, axis=0)
+    try:
+        return np.random.uniform(mini, maxi, (k, X.shape[1]))
+    except Exception:
+        return None
