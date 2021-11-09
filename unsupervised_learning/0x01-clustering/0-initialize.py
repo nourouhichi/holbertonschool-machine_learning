5ee975b05210b528da2d255e3e484773140aba80
nourouhichi <1417@holbertonschool.com>
2021-11-09 11:45:23 +0100

    add condition

--- unsupervised_learning/0x01-clustering/0-initialize.py
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -5,6 +5,8 @@ import numpy as np
 
 def initialize(X, k):
     """random centroids"""
+    if type(k) != int or k <= 0:
+        return None
     maxi = np.amax(X, axis=0)
     mini = np.amin(X, axis=0)
     try:
