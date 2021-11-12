5d6271bf858c1439c7de2e51d2c4af1981b7cf96
nourouhichi <1417@holbertonschool.com>
2021-11-12 16:34:50 +0100

    new add

--- unsupervised_learning/0x02-hmm/1-regular.py
+++ unsupervised_learning/0x02-hmm/1-regular.py
@@ -5,7 +5,9 @@ import numpy as np
 
 def regular(P):
     """stationary"""
-    if len(P.shape) != 2:
+    if type(p) != np.ndarray:
+        return None
+    if len(P.shape) != 2 or P.shape[1] != P.shape[0]:
         return None
     try:
         power = np.linalg.matrix_power(P, 100)
