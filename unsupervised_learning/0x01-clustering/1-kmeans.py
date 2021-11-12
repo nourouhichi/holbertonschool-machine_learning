25feee163c5735096641cc07e95e69528f00b257
nourouhichi <1417@holbertonschool.com>
2021-11-12 12:06:19 +0100

    new add

---
+++ unsupervised_learning/0x01-clustering/1-kmeans.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""clustering"""
+import numpy as np
+
+
+def initialize(X, k):
+    """random centroids"""
+    if type(k) != int or k <= 0:
+        return None
+    try:
+        maxi = np.amax(X, axis=0)
+        mini = np.amin(X, axis=0)
+        return np.random.uniform(mini, maxi, (k, X.shape[1]))
+    except Exception:
+        return None
+
+
+def kmeans(X, k, iterations=1000):
+    """iteration operation"""
+    if type(iterations) != int or iterations <= 0:
+        return None
+    centroids = initialize(X, k)
+    for _ in range(iterations):
+        clusters = np.argmin(np.linalg.norm(
+                             X[:, None] - centroids, axis=-1), axis=-1)
+        C = np.zeros_like(centroids)
+        for c in range(k):
+            C[c] = np.mean(X[clusters == c], axis=0)
+        if(C == centroids).all():
+            return centroids, clusters
+        centroids = C
+    clusters = np.argmin(np.linalg.norm(X[
+                         :, None] - centroids, axis=-1), axis=-1)
+    return C, clusters
