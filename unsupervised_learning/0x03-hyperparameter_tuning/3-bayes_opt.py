236116d9b9eb7bfe4ed4225ff4b038e457718cea
nourouhichi <1417@holbertonschool.com>
2021-11-18 18:28:33 +0100

    new add

---
+++ unsupervised_learning/0x03-hyperparameter_tuning/3-bayes_opt.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""bayes optim """
+import numpy as np
+GP = __import__('2-gp').GaussianProcess
+
+
+class BayesianOptimization:
+    """bayes omptim"""
+
+    def __init__(
+            self,
+            f,
+            X_init,
+            Y_init,
+            bounds,
+            ac_samples,
+            l=1,
+            sigma_f=1,
+            xsi=0.01,
+            minimize=True):
+        """ Init function"""
+        l, h = bounds
+        self.f = f
+        self.gp = GP(X_init, Y_init, l, sigma_f)
+        self.X_s = np.linspace(l, h, ac_samples).reshape((-1, 1))
+        self.xsi = xsi
+        self.minimize = minimize
---
+++ unsupervised_learning/0x03-hyperparameter_tuning/4-bayes_opt.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""bayes optim """
+import numpy as np
+GP = __import__('2-gp').GaussianProcess
+from scipy.stats import norm
+
+
+class BayesianOptimization:
+    """bayes omptim"""
+
+    def __init__(
+            self,
+            f,
+            X_init,
+            Y_init,
+            bounds,
+            ac_samples,
+            l=1,
+            sigma_f=1,
+            xsi=0.01,
+            minimize=True):
+        """ Init function"""
+        l, h = bounds
+        self.f = f
+        self.gp = GP(X_init, Y_init, l, sigma_f)
+        self.X_s = np.linspace(l, h, ac_samples).reshape((-1, 1))
+        self.xsi = xsi
+        self.minimize = minimize
+
+    def acquisition(self):
+        """next best sample location"""
+        mu, cov = self.gp.predict(self.X_s)
+        z = np.zeros(cov.shape[0])
+        if self.minimize:
+            mu_ = np.min(self.gp.Y)
+            ip = mu_ - mu - self.xsi
+        else:
+            mu_s_opt = np.max(self.gp.Y)
+            ip = mu - mu_s_opt - self.xsi
+        for i in range(cov.shape[0]):
+            if cov[i] > 0:
+                z[i] = ip[i] / cov[i]
+            else:
+                z[i] = 0
+            EI = ip * norm.cdf(z) + cov * norm.pdf(z)
+        X_next = self.X_s[np.argmax(EI)]
+        return X_next, EI
