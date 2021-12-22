6bff0654755a24df0676bad985f167c1d567a2b2
nourouhichi <1417@holbertonschool.com>
2021-12-22 21:34:04 +0100

    add try

--- unsupervised_learning/0x01-clustering/2-variance.py
+++ unsupervised_learning/0x01-clustering/2-variance.py
@@ -5,6 +5,9 @@ import numpy as np
 
 def variance(X, C):
     """total intra-cluster avriance"""
-    dist = np.argmin(np.linalg.norm((X[
-        :, np.newaxis, :] - C), axis=2), axis=1)
-    return np.linalg.norm(X - C[dist]) ** 2
+    try:
+        dist = np.argmin(np.linalg.norm((X[
+            :, np.newaxis, :] - C), axis=2), axis=1)
+        return np.linalg.norm(X - C[dist]) ** 2
+    except Exception:
+        return None
