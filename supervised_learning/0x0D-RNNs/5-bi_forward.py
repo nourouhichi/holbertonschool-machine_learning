a0d4a741889198b511c886629064d297c3b0f3b8
nourouhichi <1417@holbertonschool.com>
2021-12-22 09:58:49 +0100

    fix weight vector

--- supervised_learning/0x0D-RNNs/5-bi_forward.py
+++ supervised_learning/0x0D-RNNs/5-bi_forward.py
@@ -9,7 +9,7 @@ class BidirectionalCell:
         """init function for  cell"""
         self.Whf = np.random.randn(i + h, h)
         self.Whb = np.random.randn(i + h, h)
-        self.Wy = np.random.randn(h, o)
+        self.Wy = np.random.randn(2 * h, o)
         self.by = np.zeros((1, o))
         self.bhf = np.zeros((1, h))
         self.bhb = np.zeros((1, h))
