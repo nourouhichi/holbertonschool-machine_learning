946bee46d0a226d43ed9e7573ac073e503554f42
nourouhichi <1417@holbertonschool.com>
2021-12-23 12:40:33 +0100

    add condition

--- unsupervised_learning/0x01-clustering/3-optimum.py
+++ unsupervised_learning/0x01-clustering/3-optimum.py
@@ -8,9 +8,11 @@ variance = __import__('2-variance').variance
 def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     """optimum number of clusters"""
     try:
+        if kmax == None:
+            kmax = X.shape[0]
         results = []
         d_vars = []
-        for k in range(kmin, kmax+1):
+        for k in range(kmin, kmax + 1):
             C, clss = kmeans(X, k, iterations)
             results.append((C, clss))
             if k == kmin:
