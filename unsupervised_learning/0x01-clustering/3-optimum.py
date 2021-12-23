74d6d5dfe4b19819ded2a927e0778deb649bdbb3
nourouhichi <1417@holbertonschool.com>
2021-12-23 12:36:36 +0100

    new add

---
+++ unsupervised_learning/0x01-clustering/3-optimum.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Variance"""
+kmeans = __import__('1-kmeans').kmeans
+variance = __import__('2-variance').variance
+
+
+def optimum_k(X, kmin=1, kmax=None, iterations=1000):
+    """optimum number of clusters"""
+    try:
+        results = []
+        d_vars = []
+        for k in range(kmin, kmax+1):
+            C, clss = kmeans(X, k, iterations)
+            results.append((C, clss))
+            if k == kmin:
+                small = variance(X, C)
+            d_vars.append(small - variance(X, C))
+        return results, d_vars
+    except Exception:
+        return None, None
