5685abd3498d3249fa9f50b5080e734aa6a6732c
nourouhichi <1417@holbertonschool.com>
2021-12-25 17:06:28 +0100

    fix pycodestyle

--- supervised_learning/0x11-attention/4-positional_encoding.py
+++ supervised_learning/0x11-attention/4-positional_encoding.py
@@ -8,10 +8,10 @@ def positional_encoding(max_seq_len, dm):
     p_embeddings = np.zeros((max_seq_len, dm))
     for i in range(max_seq_len):
         for j in range(0, dm, 2):
-            #sin for even
+            # sin for even
             p_embeddings[i, j] = np.sin(
                 i / np.power(10000, (2 * j // 2) / dm))
-            #cos for odd
+            # cos for odd
             p_embeddings[i, j + 1] = np.cos(
                 i / np.power(10000, (2 * j // 2) / dm))
     return p_embeddings
