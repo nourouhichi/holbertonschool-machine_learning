b93bb76aed21f36b0617c4c324a7051188c826e3
nourouhichi <1417@holbertonschool.com>
2022-01-17 11:57:43 +0100

    new add

---
+++ supervised_learning/0x10-nlp_metrics/0-uni_bleu.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""belu score calculation"""
+import numpy as np
+
+
+def uni_bleu(references, sentence):
+    """unigram bleu score"""
+    num = 0
+    occ = []
+    length = len(sentence)
+    ref_len = np.array([len(i) for i in references])
+    ref_min = np.argmin(np.abs(ref_len - length))
+    i = len(references[ref_min])
+    if i > length:
+        pen = np.exp(1 - i / length)
+    else:
+        pen = 1
+    new_sentence = list(dict.fromkeys(sentence))
+    for i in new_sentence:
+        for y in references:
+            occ.append(y.count(i))
+        num += max(occ)
+        occ = []
+        return pen * num / length
