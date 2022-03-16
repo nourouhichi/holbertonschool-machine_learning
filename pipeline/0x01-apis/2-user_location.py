2221c6e885c64814e4112983f101047eccdf2924
nourouhichi <1417@holbertonschool.com>
2022-03-16 19:04:25 +0100

    casting

--- pipeline/0x01-apis/2-user_location.py
+++ pipeline/0x01-apis/2-user_location.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     if resp.status_code == 200:
         print(resp.json()["location"])
     elif resp.status_code == 403:
-        x = time.time() - resp.headers['X-Ratelimit-Reset']
+        x = int(time.time()) - int(resp.headers['X-Ratelimit-Reset'])
         print('Reset in {} min'.format(x))
     else:
         print('Not found')
