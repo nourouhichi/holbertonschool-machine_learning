29e45a2928bc48fc7ad02e050cf0dc6c5395e63d
nourouhichi <1417@holbertonschool.com>
2022-03-16 18:40:22 +0100

    new add

---
+++ pipeline/0x01-apis/1-sentience.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""api implimentation"""
+import requests
+
+
+def sentientPlanets():
+    """ a method that returns the list of names of the
+    home planets of all sentient species."""
+    r = 'https://swapi-api.hbtn.io/api/species/'
+    li = []
+    while r is not None:
+        page = requests.get(r).json()
+        for i in page["results"]:
+            home = i['homeworld']
+            if home is not None:
+                planet = requests.get(home).json()
+            li.append(planet["name"])
+        r = page["next"]
+    return li
